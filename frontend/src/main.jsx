8b3014df3224b5d7d05990987bdd3d65fdbff321
Md-Afzal786guru <mda890667@gmail.com>
2026-02-15 15:45:37 +0530

    Refactor import path for UserContext to use consistent casing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { UserContextProvider } from "./context/userContext.jsx";
+import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 import { ProductContextProvider } from "./context/ProductContext.jsx";
 
